d49a50e7bf89da84179ee50f20780af9494aec2e
Nikita Serba <nikitaserba@icloud.com>
2018-08-08 23:10:38 +0300

    Add copyright

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2018 Nikita S., All Rights Reserved
+ * https://github.com/Nekit10/pymutest/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3.0.
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 #include <iostream>
 
 int main() {
